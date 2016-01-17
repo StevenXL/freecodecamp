ef282e22f3be7daeb332edc1ad713a93f24e4046
Steven Leiva <leiva.steven@gmail.com>
2016-01-16 23:26:33 -0500

    Add code for the server to actually start via app.listen().

--- backEndProjects/timestamp/server.js
+++ backEndProjects/timestamp/server.js
@@ -9,3 +9,7 @@ routes(app);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+app.listen(8080, function() {
+  console.log("Listening on port 8080...");
+});
