d75108cb59745961c2ad886e070c9850e55d7454
Steven Leiva <leiva.steven@gmail.com>
2016-01-16 23:20:52 -0500

    Use express's functionality for serving static files to create virtual paths that our application can understand. These virtual paths ae now '/public' and '/controllers', which will be routed to the correct physical directory.

--- backEndProjects/timestamp/server.js
+++ backEndProjects/timestamp/server.js
@@ -3,3 +3,7 @@
 var express = require('express'),
     routes  = require('./app/routes/index.js'),
     app     = express();
+
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
